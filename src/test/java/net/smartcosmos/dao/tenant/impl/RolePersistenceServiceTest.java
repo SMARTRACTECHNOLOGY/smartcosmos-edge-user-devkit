cf1a5d18d8d5ed0b0410e118f07c4202176f033f
Jan Bauer <jan.bauer@smartrac-group.com>
2016-07-13 15:40:59 +0200

    added create role test

---
+++ src/test/java/net/smartcosmos/dao/tenant/impl/RolePersistenceServiceTest.java
@@ -0,0 +1,69 @@
+package net.smartcosmos.dao.tenant.impl;
+
+import net.smartcosmos.dao.tenant.TenantPersistenceTestApplication;
+import net.smartcosmos.ext.tenant.TenantPersistenceConfig;
+import net.smartcosmos.ext.tenant.dto.CreateOrUpdateRoleRequest;
+import net.smartcosmos.ext.tenant.dto.CreateOrUpdateRoleResponse;
+import net.smartcosmos.ext.tenant.impl.RolePersistenceService;
+import net.smartcosmos.ext.tenant.repository.RoleRepository;
+import net.smartcosmos.ext.tenant.util.UuidUtil;
+import org.junit.After;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.IntegrationTest;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.Assert.assertTrue;
+
+
+@SuppressWarnings("Duplicates")
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = {
+        TenantPersistenceTestApplication.class,
+        TenantPersistenceConfig.class})
+@ActiveProfiles("test")
+@WebAppConfiguration
+@IntegrationTest({ "spring.cloud.config.enabled=false", "eureka.client.enabled:false" })
+public class RolePersistenceServiceTest {
+
+    @Autowired
+    RolePersistenceService rolePersistenceService;
+
+    @Autowired
+    RoleRepository roleRepository;
+
+    @After
+    public void tearDown() throws Exception {
+        roleRepository.deleteAll();
+    }
+
+    private final String tenantRoleTest = UuidUtil.getTenantUrnFromUuid(UuidUtil.getNewUuid());
+
+    @Test
+    public void thatCreateRoleSucceeds() {
+        final String roleName = "createTestRole";
+        final String authority = "testAuth";
+
+        List<String> authorities = new ArrayList<>();
+        authorities.add(authority);
+
+        CreateOrUpdateRoleRequest createRole = CreateOrUpdateRoleRequest.builder()
+                .active(true)
+                .authorities(authorities)
+                .name(roleName)
+                .build();
+
+        Optional<CreateOrUpdateRoleResponse> createResponse = rolePersistenceService
+                .createRole(tenantRoleTest, createRole);
+
+        assertTrue(createResponse.isPresent());
+    }
+}
