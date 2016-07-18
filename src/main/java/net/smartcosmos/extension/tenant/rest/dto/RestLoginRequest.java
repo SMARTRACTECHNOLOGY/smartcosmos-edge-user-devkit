cde82afeda77a336e12befd2908084092ff0c596
Andreas Siegel <andreas.siegel@smartrac-group.com>
2016-07-18 16:11:23 +0200

    Add models for login

---
+++ src/main/java/net/smartcosmos/extension/tenant/rest/dto/RestLoginRequest.java
@@ -0,0 +1,21 @@
+package net.smartcosmos.extension.tenant.rest.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@Builder
+@AllArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class RestLoginRequest {
+
+    private static final int VERSION = 1;
+    private final int version = VERSION;
+
+    private String username;
+    private String password;
+}
---
+++ src/main/java/net/smartcosmos/extension/tenant/rest/dto/RestLoginResponse.java
@@ -0,0 +1,36 @@
+package net.smartcosmos.extension.tenant.rest.dto;
+
+import java.beans.ConstructorProperties;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonIgnoreProperties({ "version" })
+@Builder
+public class RestLoginResponse {
+
+    private static final int VERSION = 1;
+    private final int version = VERSION;
+
+    private final String urn;
+    private final String username;
+    private final List<String> authorities;
+    private final String tenantUrn;
+
+    @ConstructorProperties({ "urn", "username", "authorities", "tenantUrn" })
+    public RestLoginResponse(String urn, String username, List<String> authorities, String tenantUrn) {
+        this.urn = urn;
+        this.username = username;
+        this.authorities = new ArrayList<>();
+        if (authorities != null && !authorities.isEmpty()) {
+            this.authorities.addAll(authorities);
+        }
+        this.tenantUrn = tenantUrn;
+    }
+}
