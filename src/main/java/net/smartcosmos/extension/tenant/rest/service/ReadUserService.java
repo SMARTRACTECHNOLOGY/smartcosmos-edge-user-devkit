3fa3f1f1f038dc35f9abb2d8afeae541005ccbe1
Tim Cross <tim.cross@smartrac-group.com>
2016-07-19 15:58:48 +0200

    code, tests for readUser by name and by urn

---
+++ src/main/java/net/smartcosmos/extension/tenant/rest/service/ReadUserService.java
@@ -0,0 +1,60 @@
+package net.smartcosmos.extension.tenant.rest.service;
+
+import java.util.Optional;
+import javax.inject.Inject;
+
+import lombok.extern.slf4j.Slf4j;
+
+import org.springframework.core.convert.ConversionService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import net.smartcosmos.events.SmartCosmosEventTemplate;
+import net.smartcosmos.extension.tenant.dao.RoleDao;
+import net.smartcosmos.extension.tenant.dao.TenantDao;
+import net.smartcosmos.extension.tenant.dto.GetOrDeleteUserResponse;
+
+@Slf4j
+@Service
+public class ReadUserService extends AbstractTenantService {
+
+    @Inject
+    public ReadUserService(
+        TenantDao tenantDao,
+        RoleDao roleDao,
+        SmartCosmosEventTemplate smartCosmosEventTemplate,
+        ConversionService conversionService) {
+
+        super(tenantDao, roleDao, smartCosmosEventTemplate, conversionService);
+    }
+
+    public ResponseEntity<?> findByUrn(String urn) {
+
+        Optional<GetOrDeleteUserResponse> entity = tenantDao.findUserByUrn(urn);
+
+        if (entity.isPresent()) {
+            // TODO: send event user:read
+            return ResponseEntity
+                .ok()
+                .body(conversionService.convert(entity.get(), GetOrDeleteUserResponse.class));
+        }
+
+        // TODO: send event tenant:notFound
+        return ResponseEntity.notFound().build();
+    }
+
+    public ResponseEntity<?> findByName(String name) {
+
+        Optional<GetOrDeleteUserResponse> entity = tenantDao.findUserByName(name);
+
+        if (entity.isPresent()) {
+            // TODO: send event tenant:read
+            return ResponseEntity
+                .ok()
+                .body(conversionService.convert(entity.get(), GetOrDeleteUserResponse.class));
+        }
+
+        // TODO: send event tenant:notFound
+        return ResponseEntity.notFound().build();
+    }
+}
--- src/test/java/net/smartcosmos/extension/tenant/rest/resource/ReadTenantResourceTest.java
+++ src/test/java/net/smartcosmos/extension/tenant/rest/resource/ReadTenantResourceTest.java
@@ -19,7 +19,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-public class GetTenantResourceTest extends AbstractTestResource {
+public class ReadTenantResourceTest extends AbstractTestResource {
 
     @Autowired
     protected TenantDao tenantDao;
