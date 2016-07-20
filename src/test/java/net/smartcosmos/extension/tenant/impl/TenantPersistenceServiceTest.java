6dc241180b1ffffea1d6a9bad0e311ff337ccdb7
Andreas Siegel <andreas.siegel@smartrac-group.com>
2016-07-20 09:47:31 +0200

    Add find user tests

--- src/test/java/net/smartcosmos/extension/tenant/impl/TenantPersistenceServiceTest.java
+++ src/test/java/net/smartcosmos/extension/tenant/impl/TenantPersistenceServiceTest.java
@@ -5,7 +5,6 @@ import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
-import org.apache.commons.lang.RandomStringUtils;
 import org.junit.*;
 import org.junit.runner.*;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -655,6 +654,83 @@ public class TenantPersistenceServiceTest {
 //        assertEquals(roles.size(),userResponse2.get().getRoles().size());
 //        assertEquals(surname, userResponse2.get().getSurname());
 //        assertEquals(username, userResponse2.get().getUsername());
+    }
+
+    @Test
+    public void thatFindUserByUserNameSucceeds() throws Exception {
+
+        String username = "FindByUsernameTestUser";
+        String emailAddress = "username.user@example.com";
+        String givenName = "John";
+        String surname = "Doe";
+
+        List<String> roles = new ArrayList<>();
+        roles.add("Admin");
+
+        CreateUserRequest userRequest = CreateUserRequest.builder()
+            .username(username)
+            .active(true)
+            .emailAddress(emailAddress)
+            .roles(roles)
+            .givenName(givenName)
+            .surname(surname)
+            .tenantUrn(testUserTenantUrn)
+            .build();
+        CreateOrUpdateUserResponse createResponse = tenantPersistenceService.createUser(userRequest).get();
+
+        Optional<GetOrDeleteUserResponse> getResponse = tenantPersistenceService.findUserByName(username);
+
+        assertTrue(getResponse.isPresent());
+        assertEquals(createResponse.getUrn(), getResponse.get().getUrn());
+        assertEquals(createResponse.getActive(), getResponse.get().getActive());
+        assertNotNull(getResponse.get().getActive());
+        assertEquals(createResponse.getRoles(), getResponse.get().getRoles());
+        assertEquals(username, getResponse.get().getUsername());
+        assertEquals(emailAddress, getResponse.get().getEmailAddress());
+        assertEquals(givenName, getResponse.get().getGivenName());
+        assertEquals(surname, getResponse.get().getSurname());
+        assertEquals(testUserTenantUrn, getResponse.get().getTenantUrn());
+    }
+
+    @Test
+    public void thatFindUserByUrnSucceeds() throws Exception {
+
+        String username = "FindByUrnTestUser";
+        String emailAddress = "urn.user@example.com";
+        String givenName = "John";
+        String surname = "Doe";
+
+        List<String> roles = new ArrayList<>();
+        roles.add("Admin");
+
+        CreateUserRequest userRequest = CreateUserRequest.builder()
+            .username(username)
+            .active(true)
+            .emailAddress(emailAddress)
+            .roles(roles)
+            .givenName(givenName)
+            .surname(surname)
+            .tenantUrn(testUserTenantUrn)
+            .build();
+        CreateOrUpdateUserResponse createResponse = tenantPersistenceService.createUser(userRequest).get();
+
+        Optional<GetOrDeleteUserResponse> getResponse = tenantPersistenceService.findUserByName(username);
+
+        assertTrue(getResponse.isPresent());
+        assertEquals(createResponse.getUrn(), getResponse.get().getUrn());
+        assertEquals(createResponse.getActive(), getResponse.get().getActive());
+        assertNotNull(getResponse.get().getActive());
+        assertEquals(createResponse.getRoles(), getResponse.get().getRoles());
+        assertEquals(username, getResponse.get().getUsername());
+        assertEquals(emailAddress, getResponse.get().getEmailAddress());
+        assertEquals(givenName, getResponse.get().getGivenName());
+        assertEquals(surname, getResponse.get().getSurname());
+        assertEquals(testUserTenantUrn, getResponse.get().getTenantUrn());
+    }
+
+    @Test
+    public void thatUpdateUpserSucceeds() throws Exception {
+        // TODO updateUser(UpdateUserRequest updateUserRequest)
     }
 
     // endregion
