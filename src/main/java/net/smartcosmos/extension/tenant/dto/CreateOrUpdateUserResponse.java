90197ed135ec3ac41f2ab63c385f67c6ad13ca56
Andreas Siegel <andreas.siegel@smartrac-group.com>
2016-07-20 08:00:24 +0200

    Clean up CreateOrUpdateUserResponse

--- src/main/java/net/smartcosmos/extension/tenant/dto/CreateOrUpdateUserResponse.java
+++ src/main/java/net/smartcosmos/extension/tenant/dto/CreateOrUpdateUserResponse.java
@@ -1,23 +1,22 @@
 package net.smartcosmos.extension.tenant.dto;
 
-import java.beans.ConstructorProperties;
 import java.util.List;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
-import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
-import lombok.Setter;
 
 @Data
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @JsonIgnoreProperties({ "version" })
+@Builder
+@AllArgsConstructor
 public class CreateOrUpdateUserResponse {
 
     private static final int VERSION = 1;
-    @Setter(AccessLevel.NONE)
-    private int version = VERSION;
+    private final int version = VERSION;
 
     private final String urn;
     private final String tenantUrn;
@@ -28,22 +27,4 @@ public class CreateOrUpdateUserResponse {
     private final String password;
     private final List<String> roles;
     private final Boolean active;
-
-    @Builder
-    @ConstructorProperties({ "urn", "tenantUrn", "username", "emailAddress", "givenName", "surname", "password", "roles", "active" })
-    public CreateOrUpdateUserResponse(
-        String urn, String tenantUrn, String username, String emailAddress, String givenName, String surname, String password,
-        List<String> roles, List<String> authorities, Boolean active) {
-        this.urn = urn;
-        this.tenantUrn = tenantUrn;
-        this.username = username;
-        this.emailAddress = emailAddress;
-        this.givenName = givenName;
-        this.surname = surname;
-        this.password = password;
-        this.roles = roles;
-        this.active = active;
-
-        this.version = VERSION;
-    }
 }
