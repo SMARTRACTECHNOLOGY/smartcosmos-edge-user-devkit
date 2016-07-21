b6ba81317d2c41b04a3e8886ec5e367d858f84da
Andreas Siegel <andreas.siegel@smartrac-group.com>
2016-07-21 09:52:02 +0200

    Add SmartCosmosUser to resources

--- src/main/java/net/smartcosmos/extension/tenant/rest/resource/ReadUserResource.java
+++ src/main/java/net/smartcosmos/extension/tenant/rest/resource/ReadUserResource.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import net.smartcosmos.extension.tenant.rest.service.ReadUserService;
 import net.smartcosmos.security.EndpointMethodControl;
+import net.smartcosmos.security.user.SmartCosmosUser;
 import net.smartcosmos.spring.SmartCosmosRdao;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON_UTF8_VALUE;
@@ -29,7 +30,7 @@ public class ReadUserResource {
     @RequestMapping(value = "/users/{urn}", method = RequestMethod.GET, produces = APPLICATION_JSON_UTF8_VALUE)
     @EndpointMethodControl(key = "tenant.urn.get")
     @ConditionalOnProperty(prefix = "smt.endpoints.user.urn.get", name = "enabled", matchIfMissing = true)
-    public ResponseEntity<?> getByUrn(@PathVariable String urn) {
+    public ResponseEntity<?> getByUrn(@PathVariable String urn, SmartCosmosUser user) {
 
         return readUserService.findByUrn(urn);
     }
@@ -38,7 +39,7 @@ public class ReadUserResource {
     @EndpointMethodControl(key = "tenant.get")
     @ConditionalOnProperty(prefix = "smt.endpoints.user.get", name = "enabled", matchIfMissing = true)
     public ResponseEntity<?> getByName(
-        @RequestParam(value = "name") String name) {
+        @RequestParam(value = "name") String name, SmartCosmosUser user) {
 
         return readUserService.findByName(name);
     }
