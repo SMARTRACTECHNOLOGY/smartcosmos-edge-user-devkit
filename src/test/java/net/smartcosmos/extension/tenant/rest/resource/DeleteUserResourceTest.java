77d2abe15ec8fd09dadd631e173093742b8ddf35
Andreas Siegel <andreas.siegel@smartrac-group.com>
2016-07-23 12:04:03 +0200

    Add delete user not found resource test

--- src/test/java/net/smartcosmos/extension/tenant/rest/resource/DeleteUserResourceTest.java
+++ src/test/java/net/smartcosmos/extension/tenant/rest/resource/DeleteUserResourceTest.java
@@ -1,17 +1,17 @@
 package net.smartcosmos.extension.tenant.rest.resource;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
-
+import net.smartcosmos.extension.tenant.TenantPersistenceTestApplication;
+import net.smartcosmos.extension.tenant.dao.TenantDao;
 import net.smartcosmos.extension.tenant.dto.user.UserResponse;
-import org.junit.*;
+import net.smartcosmos.extension.tenant.util.UuidUtil;
+import org.junit.After;
+import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.web.servlet.MvcResult;
 
-import net.smartcosmos.extension.tenant.TenantPersistenceTestApplication;
-import net.smartcosmos.extension.tenant.dao.TenantDao;
-import net.smartcosmos.extension.tenant.util.UuidUtil;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.asyncDispatch;
@@ -20,7 +20,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 /**
- * Unit Testing sample for creating Tenants.
+ * Unit Testing sample for deleting Users.
  */
 @SuppressWarnings("Duplicates")
 @org.springframework.boot.test.SpringApplicationConfiguration(classes = { TenantPersistenceTestApplication.class })
@@ -37,7 +37,7 @@ public class DeleteUserResourceTest extends AbstractTestResource {
     }
 
     /**
-     * Test that creating a Tenant is successful.
+     * Test that deleting a User is successful.
      *
      * @throws Exception
      */
@@ -71,6 +71,29 @@ public class DeleteUserResourceTest extends AbstractTestResource {
 
         this.mockMvc.perform(asyncDispatch(mvcResult))
             .andExpect(status().isNoContent());
+
+    }
+
+    /**
+     * Test that deleting a nonexistent User fails.
+     *
+     * @throws Exception
+     */
+    @Test
+    public void thatDeleteNonexistentUserFails() throws Exception {
+
+        final String expectedUserUrn = "urn:user:uuid:" + UuidUtil.getNewUuid().toString();
+
+        when(tenantDao.deleteUserByUrn(anyString(), anyString())).thenReturn(Optional.empty());
+
+        MvcResult mvcResult = this.mockMvc.perform(
+                delete("/users/" + expectedUserUrn).contentType(contentType))
+                .andExpect(status().isOk())
+                .andExpect(request().asyncStarted())
+                .andReturn();
+
+        this.mockMvc.perform(asyncDispatch(mvcResult))
+                .andExpect(status().isNotFound());
 
     }
 }
