d8300dfc3fbb87ab02ccdcf2d6db01bce297fecc
Andreas Siegel <andreas.siegel@smartrac-group.com>
2016-08-10 18:17:46 +0200

    Replace @EndpointMethodControl by @ConditionalOnProperty, use constants for Tenant endpoints

--- src/main/java/net/smartcosmos/extension/tenant/rest/resource/tenant/CreateTenantResource.java
+++ src/main/java/net/smartcosmos/extension/tenant/rest/resource/tenant/CreateTenantResource.java
@@ -16,17 +16,21 @@ import org.springframework.web.context.request.async.DeferredResult;
 import net.smartcosmos.annotation.SmartCosmosRdao;
 import net.smartcosmos.extension.tenant.rest.dto.tenant.RestCreateTenantRequest;
 import net.smartcosmos.extension.tenant.rest.service.tenant.CreateTenantService;
-import net.smartcosmos.security.EndpointMethodControl;
 import net.smartcosmos.security.user.SmartCosmosUser;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON_UTF8_VALUE;
+
+import static net.smartcosmos.extension.tenant.rest.resource.BasicEndpointConstants.ENDPOINT_ENABLEMENT_PROPERTY_ENABLED;
+import static net.smartcosmos.extension.tenant.rest.resource.tenant.TenantEndpointConstants.ENDPOINT_ENABLEMENT_TENANTS;
+import static net.smartcosmos.extension.tenant.rest.resource.tenant.TenantEndpointConstants.ENDPOINT_ENABLEMENT_TENANTS_CREATE;
+import static net.smartcosmos.extension.tenant.rest.resource.tenant.TenantEndpointConstants.ENDPOINT_TENANTS;
 
 /**
  * Initially created by SMART COSMOS Team on July 01, 2016.
  */
 @SmartCosmosRdao
 @Slf4j
-@ConditionalOnProperty(prefix = "smt.endpoints.tenant", name = "enabled", matchIfMissing = true)
+@ConditionalOnProperty(prefix = ENDPOINT_ENABLEMENT_TENANTS, name = ENDPOINT_ENABLEMENT_PROPERTY_ENABLED, matchIfMissing = true)
 //@Api
 public class CreateTenantResource {
 
@@ -35,9 +39,11 @@ public class CreateTenantResource {
     @Autowired
     public CreateTenantResource(CreateTenantService service) { this.service = service; }
 
-    @RequestMapping(value = "/tenants", method = RequestMethod.POST, produces = APPLICATION_JSON_UTF8_VALUE, consumes = APPLICATION_JSON_UTF8_VALUE)
-    @EndpointMethodControl(key = "tenant.post")
-    @ConditionalOnProperty(prefix = "smt.endpoints.tenant.post", name = "enabled", matchIfMissing = true)
+    @RequestMapping(value = ENDPOINT_TENANTS,
+                    method = RequestMethod.POST,
+                    produces = APPLICATION_JSON_UTF8_VALUE,
+                    consumes = APPLICATION_JSON_UTF8_VALUE)
+    @ConditionalOnProperty(prefix = ENDPOINT_ENABLEMENT_TENANTS_CREATE, name = ENDPOINT_ENABLEMENT_PROPERTY_ENABLED, matchIfMissing = true)
     public DeferredResult<ResponseEntity> createTenant(
         @RequestBody @Valid RestCreateTenantRequest restCreateTenantRequest,
         @AuthenticationPrincipal SmartCosmosUser user) {
---
+++ src/main/java/net/smartcosmos/extension/tenant/rest/resource/tenant/TenantEndpointConstants.java
@@ -0,0 +1,43 @@
+package net.smartcosmos.extension.tenant.rest.resource.tenant;
+
+import static net.smartcosmos.extension.tenant.rest.resource.BasicEndpointConstants.ENDPOINT_ENABLEMENT_PREFIX;
+
+/**
+ * A constant interface class for the Tenant endpoints.
+ */
+public interface TenantEndpointConstants {
+
+    // region Base Constants
+
+    String ENDPOINT_BASE_NAME_TENANTS = "tenants";
+
+    String ENDPOINT_ENABLEMENT_TENANTS = ENDPOINT_ENABLEMENT_PREFIX + "." + ENDPOINT_BASE_NAME_TENANTS;
+
+    // endregion
+
+    // region Path Segment and Parameter Constants
+
+    String TENANT_URN = "tenantUrn";
+    String NAME = "name";
+
+    // endregion
+
+    // region Resource Paths
+
+    String ENDPOINT_TENANTS = "/{" + ENDPOINT_BASE_NAME_TENANTS + "}";
+    String ENDPOINT_TENANTS_URN = ENDPOINT_TENANTS + "/{" + TENANT_URN + "}";
+
+    // endregion
+
+    // region Endpoint Enablement
+
+    String ENDPOINT_ENABLEMENT_TENANTS_CREATE = ENDPOINT_ENABLEMENT_TENANTS + ".create";
+
+    String ENDPOINT_ENABLEMENT_TENANTS_READ = ENDPOINT_ENABLEMENT_TENANTS + ".read";
+    String ENDPOINT_ENABLEMENT_TENANTS_READ_ALL = ENDPOINT_ENABLEMENT_TENANTS_READ + ".all";
+    String ENDPOINT_ENABLEMENT_TENANTS_READ_URN = ENDPOINT_ENABLEMENT_TENANTS_READ + ".urn";
+
+    String ENDPOINT_ENABLEMENT_TENANTS_UPDATE = ENDPOINT_ENABLEMENT_TENANTS + ".update";
+
+    // endregion
+}
