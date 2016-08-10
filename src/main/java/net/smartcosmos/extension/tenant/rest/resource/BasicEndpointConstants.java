cd3f7d05bce0acc8b93110239953799f662fd7d7
Andreas Siegel <andreas.siegel@smartrac-group.com>
2016-08-10 18:07:59 +0200

    Replace @EndpointMethodControl by @ConditionalOnProperty, use constants for Read Roles

---
+++ src/main/java/net/smartcosmos/extension/tenant/rest/resource/BasicEndpointConstants.java
@@ -0,0 +1,10 @@
+package net.smartcosmos.extension.tenant.rest.resource;
+
+/**
+ * A constant interface class for all endpoints.
+ */
+public interface BasicEndpointConstants {
+
+    String ENDPOINT_ENABLEMENT_PREFIX = "smartcosmos.endpoints";
+    String ENDPOINT_ENABLEMENT_PROPERTY_ENABLED = "enabled";
+}
---
+++ src/main/java/net/smartcosmos/extension/tenant/rest/resource/role/RoleEndpointConstants.java
@@ -0,0 +1,44 @@
+package net.smartcosmos.extension.tenant.rest.resource.role;
+
+import static net.smartcosmos.extension.tenant.rest.resource.BasicEndpointConstants.ENDPOINT_ENABLEMENT_PREFIX;
+
+/**
+ * A constant interface class for the Role endpoints.
+ */
+public interface RoleEndpointConstants {
+
+    // region Base Constants
+
+    String ENDPOINT_BASE_NAME_ROLES = "roles";
+    String ENDPOINT_ENABLEMENT_ROLES = ENDPOINT_ENABLEMENT_PREFIX + "." + ENDPOINT_BASE_NAME_ROLES;
+
+    // endregion
+
+    // region Path Segment and Parameter Constants
+
+    String ROLE_URN = "roleUrn";
+    String NAME = "name";
+
+    // endregion
+
+    // region Resource Paths
+
+    String ENDPOINT_ROLES = "/{" + ENDPOINT_BASE_NAME_ROLES + "}";
+    String ENDPOINT_ROLES_URN = ENDPOINT_ROLES + "/{" + ROLE_URN + "}";
+
+    // endregion
+
+    // region Endpoint Enablement
+
+    String ENDPOINT_ENABLEMENT_ROLES_CREATE = ENDPOINT_ENABLEMENT_ROLES + ".create";
+
+    String ENDPOINT_ENABLEMENT_ROLES_READ = ENDPOINT_ENABLEMENT_ROLES + ".read";
+    String ENDPOINT_ENABLEMENT_ROLES_READ_ALL = ENDPOINT_ENABLEMENT_ROLES_READ + ".all";
+    String ENDPOINT_ENABLEMENT_ROLES_READ_URN = ENDPOINT_ENABLEMENT_ROLES_READ + ".urn";
+
+    String ENDPOINT_ENABLEMENT_ROLES_UPDATE = ENDPOINT_ENABLEMENT_ROLES + ".update";
+
+    String ENDPOINT_ENABLEMENT_ROLES_DELETE = ENDPOINT_ENABLEMENT_ROLES + ".delete";
+
+    // endregion
+}
