2c020fecb6d9d266aa6666ff2e5886862ebbfdbf
Andreas Siegel <andreas.siegel@smartrac-group.com>
2017-01-13 13:01:21 +0100

    Prepare separate request models for create and update

---
+++ src/main/java/net/smartcosmos/usermanagement/user/dto/UpdateUserRequest.java
@@ -0,0 +1,11 @@
+package net.smartcosmos.usermanagement.user.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Data;
+
+@Data
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonIgnoreProperties({ "version" })
+public class UpdateUserRequest {
+}
