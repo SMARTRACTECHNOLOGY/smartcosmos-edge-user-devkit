3959c88c146e057fa82ddf3be511701e379bac77
Andreas Siegel <andreas.siegel@smartrac-group.com>
2017-01-17 11:54:31 +0100

    Add Javadoc

--- src/main/java/net/smartcosmos/usermanagement/tenant/service/UpdateTenantService.java
+++ src/main/java/net/smartcosmos/usermanagement/tenant/service/UpdateTenantService.java
@@ -6,7 +6,18 @@ import org.springframework.web.context.request.async.DeferredResult;
 import net.smartcosmos.security.user.SmartCosmosUser;
 import net.smartcosmos.usermanagement.tenant.dto.UpdateTenantRequest;
 
+/**
+ * Methods to update Tenants.
+ */
 public interface UpdateTenantService {
 
+    /**
+     * Updates the Tenant defined by its URN.
+     *
+     * @param response the DeferredResult containing a response, if there is one
+     * @param tenantUrn the URN of the Tenant to update
+     * @param updateTenantRequest the request object
+     * @param user the current logged in user
+     */
     void update(DeferredResult<ResponseEntity<?>> response, String tenantUrn, UpdateTenantRequest updateTenantRequest, SmartCosmosUser user);
 }
